65ea8d9edd9a724255a8e59d668212d7170525dc
castle <castle@castleui-iMac.local>
2021-11-19 22:00:08 +0900

    #2.3 AppLoading part Three

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Image, Text } from "react-native";
+import React, { useState } from "react";
 import * as Font from "expo-font";
+import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
-  const [ready, setRedady] = useState(false);
-
-  const onFinish = () => setRedady(true);
+  const [assets] = useAssets([require("./my-face.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./my-face.jpeg"));
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./my-face.jpeg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
-
   if (!ready) {
     return (
       <AppLoading
