e34202e74d235895dc420ca9e9c19b0a52745ece
castle <castle@castleui-iMac.local>
2021-11-25 19:09:51 +0900

    #3.3 Movies Screen part Two

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,28 +1,54 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
-import { Dimensions } from "react-native";
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import { BlurView } from "expo-blur";
+import { makeImagePath } from "../utils";
 
 const API_KEY = "fd3e58ab6fece92b8142b427a386d7b1";
 
-const Container = styled.ScrollView`
-  background-color: ${(props) => props.theme.mainBgColor};
-`;
+const Container = styled.ScrollView``;
 
 const View = styled.View`
   flex: 1;
 `;
 
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text``;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const getNowPlaying = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=kr`
-    );
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=kr`
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
   };
-  return (
+
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
@@ -30,10 +56,17 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         controlsEnabled={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImagePath(movie.backdrop_path) }}
+            />
+            <BlurView intensity={80} style={StyleSheet.absoluteFill}>
+              <Title>{movie.original_title}</Title>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
