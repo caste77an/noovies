ae014993019a11197e6cea74d00a4eca54c9d28d
castle <castle@castleui-iMac.local>
2021-11-29 20:25:52 +0900

    #3.15 Typescript

--- api.ts
+++ api.ts
@@ -1,7 +1,33 @@
 const API_KEY = "fd3e58ab6fece92b8142b427a386d7b1";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-interface Movie {}
+export interface Moive{
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface BaseResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+}
+
+export interface MovieResponse extends BaseResponse {
+  results: Moive[];
+
+}
 
 const trending = () =>
   fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
