be2e77f344090355aa8b09e296d1c4af19fb3e38
caste77an <hancastellan@naver.com>
2021-12-18 12:42:32 +0900

    #3.20 Search Screen part Three

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Detail from "../screens/Detail";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => (
-  <NativeStack.Navigator>
-    <NativeStack.Screen />
+  <NativeStack.Navigator
+    screenOptions={{
+      headerBackTitleVisible: false,
+    }}
+  >
+    <NativeStack.Screen name="Detail" component={Detail} />
   </NativeStack.Navigator>
 );
 
