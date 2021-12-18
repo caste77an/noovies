5088ac19dc5158c8241b0ab8b28da596180495d5
caste77an <hancastellan@naver.com>
2021-12-18 21:43:11 +0900

    #3.26 Sharing

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Dimensions, Linking, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, Share, Platform } from "react-native";
 import styled from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Movie, moviesApi, TV, tvApi } from "../api";
@@ -11,6 +11,7 @@ import { useQuery } from "react-query";
 import Loader from "../components/Loader";
 import { Ionicons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -37,13 +38,16 @@ const Title = styled.Text`
   margin-left: 15px;
   font-weight: 500;
 `;
+
 const Data = styled.View`
   padding: 0px 20px;
 `;
+
 const Overview = styled.Text`
   color: ${(props) => props.theme.textColor};
   margin: 20px 0px;
 `;
+
 const VideoBtn = styled.TouchableOpacity`
   flex-direction: row;
 `;
@@ -66,10 +70,37 @@ const Detail: React.FC<DetailScreenProps> = ({
   route: { params },
 }) => {
   const isMovie = "original_title" in params;
-
   const { isLoading, data } = useQuery(
     [isMovie ? "movies" : "tv", params.id],
     isMovie ? moviesApi.detail : tvApi.detail
+  );
+  const shareMedia = async () => {
+    const isAndroid = Platform.OS === "android";
+    const homepage = isMovie
+      ? `https://www.imdb.com/title/${data.imdb_id}/`
+      : data.homepage;
+    if (isAndroid) {
+      await Share.share({
+        message: `${params.overview}\nCheck it out: ${homepage}`,
+        title:
+          "original_title" in params
+            ? params.original_title
+            : params.original_name,
+      });
+    } else {
+      await Share.share({
+        url: homepage,
+        title:
+          "original_title" in params
+            ? params.original_title
+            : params.original_name,
+      });
+    }
+  };
+  const ShareButton = () => (
+    <TouchableOpacity onPress={shareMedia}>
+      <Ionicons name="share-outline" color="white" size={24} />
+    </TouchableOpacity>
   );
 
   useEffect(() => {
@@ -77,9 +108,15 @@ const Detail: React.FC<DetailScreenProps> = ({
       title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
-
+  useEffect(() => {
+    if (data) {
+      setOptions({
+        headerRight: () => <ShareButton />,
+      });
+    }
+  }, [data]);
   const openYTLink = async (videoID: string) => {
-    const baseUrl = `http://m.youtube.com/watch?v=${videoID}`;
+    const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
     // await Linking.openURL(baseUrl);
     await WebBrowser.openBrowserAsync(baseUrl);
   };
